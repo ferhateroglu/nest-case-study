31d93eb27912b98eb03f9b6bbf48ab79272a6889
Ferhat Eroglu <erogluferhat@outlook.com>
2024-06-15 14:23:58 +0300

    chore: Add TypeORM configuration and global configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +9,19 @@ import { BranchesModule } from './branches/branches.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, BranchesModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getTypeOrmConfig(configService),
+    }),
+    AuthModule,
+    BranchesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
